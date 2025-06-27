80d7980fe8ab692718ef723b61506ec1bdc085f6
saltyseadawg <sabrinayu7@gmail.com>
2025-06-26 23:27:15 -0400

    add models for dictionary headwords

--- app/models.py
+++ app/models.py
@@ -1,8 +1,24 @@
 from pydantic import BaseModel
+from typing import Optional
+import abc
 
-class HeadWord(BaseModel):
-    """
+class DictEntry(BaseModel, abc.ABC):
+    id: int
+    lang: str
+    pos: Optional[str] = None
+    definition: str
+    examples: Optional[list[str]] = None
+    source: Optional[str]
     
-    """
-    pass
+class HeadWord(BaseModel, abc.ABC):
+    headword: str
+    definitions: list[DictEntry]
+    romanization: str
 
+    @abc.abstractmethod
+    def __init__(self, x, y):
+        pass
+    
+    @abc.abstractmethod
+    def set_romanization(self):
+        pass
