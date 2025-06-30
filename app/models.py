d98e7c731e17c794c2344c1a805125bc7fef7f9e
Sorano <sabrinayu7@gmail.com>
2025-06-29 21:53:50 -0400

    Setup basic REST API to get mock data (#1)
    * outlining api commands
    
    * requirements/
    
    * add models for dictionary headwords
    
    * mock reponse when querying word "graze"

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
-class Headword():
-    pass
+from pydantic import BaseModel
+from typing import Optional
+import abc
 
+class DictEntry(BaseModel, abc.ABC):
+    id: int
+    lang: str
+    pos: Optional[str] = None
+    definition: str
+    examples: Optional[list[str]] = None
+    source: Optional[str] = None
+    
+class HeadWord(BaseModel, abc.ABC):
+    headword: str
+    romanization: Optional[str]=None
+    definitions: list[DictEntry]
+    
+    @abc.abstractmethod
+    def set_romanization(self):
+        pass
+
+class TamilHeadword(HeadWord):
+    def set_romanization(self):
+        pass
