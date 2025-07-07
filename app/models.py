c0f8f9d201925a0573c439dc38ee04ac84aaf96b
shankhalika-katya <shankha221@gmail.com>
2025-07-07 16:56:41 -0400

    resolving SY review comments

--- app/models.py
+++ app/models.py
@@ -6,26 +6,25 @@ class DictEntry(BaseModel, abc.ABC):
     id: int
     lang: str
     pos: Optional[str] = None
-    primary_definition: str
-    secondary_definition: Optional[str] = None
-    tertiary_definition: Optional[str] = None
-    examples: Optional[list[str]] = None
+    definitions: list[str]
+    examples: Optional[list[int]] = None # list of ids that reference an example containing the word (examples would be in their own table in a DB)
     source: Optional[str] = None
     
 class InputWord(BaseModel, abc.ABC):
-    headword: str
+    user_input = str
     romanization: Optional[str]=None
     root: str
-    root_definitions: list[DictEntry]
+    root_definition: DictEntry 
     secondary_root_definitions: Optional[list[DictEntry]] # stretch goal to segment compound words; for now want to just capture them unsegmented
     prefixal_material: Optional[str]=None # to get passed to gramble for parsing
     suffixal_material: Optional[str]=None # to get passed to gramble for parsing
+    infixal_material: Optional[str]=None # not relevant for Tamil
     pos: list[DictEntry] # for SS to think about, bc root and derived word can have diff POS. can leave for now
     
     @abc.abstractmethod
     def set_romanization(self):
         pass
-
+    
 class TamilHeadword(InputWord):
     def set_romanization(self):
         pass
