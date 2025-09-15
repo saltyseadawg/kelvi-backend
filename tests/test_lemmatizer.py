a2b78437478094ffca9e8214831a0b1e64fa0782
shankhalika-katya <shankha221@gmail.com>
2025-09-15 16:21:27 -0400

    initial test file for lemmatizer

--- tests/test_lemmatizer.py
+++ tests/test_lemmatizer.py
@@ -15,6 +15,24 @@ class TestStringMethods(unittest.TestCase):
             for word in sent.words:
                 root = word.lemma
         self.assertEqual(root, 'அறி')
+
+    def test_imperative(self):
+        nlp = stanza.Pipeline(lang='ta', processors='tokenize,mwt,pos,lemma')
+        imperatives = {'பண்ணு': 'பண்ணு',
+        'பண்ணாதே': 'பண்ணு',
+        'பண்ணுங்கள்': 'பண்ணு',
+        'பண்ணாதீர்கள்': 'பண்ணு',
+        'பண்ணேன்': 'பண்ணு',
+        'பண்ணாதேயேன்': 'பண்ணு',
+        'பண்ணுங்களேன்': 'பண்ணு',
+        'பண்ணாதீர்களேன்': 'பண்ணு',
+        'வாயேன்': 'வா'}
+        for imperative in imperatives:
+            doc = nlp(imperative)
+            for sent in doc.sentences:
+                for word in sent.words:
+                    root = word.lemma
+            self.assertEqual(root, imperatives[imperative])
     
 
 
