cd245981cdfb8f49ed470530347ac780e8121597
saltyseadawg <sabrinayu7@gmail.com>
2025-09-23 12:12:15 -0400

    add stemmer comment

--- app/morphology/stemmer.py
+++ app/morphology/stemmer.py
@@ -1,6 +1,9 @@
 import Stemmer
 
 def stem_word(word: str, lang: str="tamil") -> str:
+    """Will need to modify the stemmer so that it stems according to 
+    our pipeline needs i.e. சாப்பிட்டேன் -> சாப்பிட் instead of சாப்பிடு  
+    """
     stemmer = Stemmer.Stemmer(lang)
     stem = stemmer.stemWord(word)
     return stem
