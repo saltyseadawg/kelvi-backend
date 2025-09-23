93a32f530acb95702b9fe1fdcda13746e74cdbce
saltyseadawg <sabrinayu7@gmail.com>
2025-09-23 12:15:59 -0400

    add stemmer test

--- tests/test_stemmer.py
+++ tests/test_stemmer.py
@@ -5,4 +5,11 @@ def test_stem_word():
     stem = stemmer.stem_word(word)
     expected = 'இரு'
     assert stem == expected
-    
+
+def test_adjusted_stemmer_output():
+    # word = 'சாப்பிட்டேன்' 
+    # stem = stemmer.stem_word(word)
+    # expected = 'சாப்பிட்'
+    # doesn't work right now
+    # assert stem == expected
+    pass
