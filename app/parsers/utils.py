1c410877dbf349e04cf31ca153955a51bd55d132
saltyseadawg <sabrinayu7@gmail.com>
2025-12-03 11:55:00 -0500

    add function for removing trailing brackets

--- app/parsers/utils.py
+++ app/parsers/utils.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 import shutil
+import re
 
+def remove_trailing_brackets(text: str) -> str:
+    """Used ChatGPT"""
+    pattern = r'(?:\s*\([^)]*\))*\s*$'
+    processed = re.sub(pattern, '', text)
+    return processed
 
 def list_files(folder_path: str) -> tuple:
     return tuple(str(x) for x in Path(folder_path).iterdir() if x.is_file())
---
+++ tests/test_utils.py
@@ -0,0 +1,12 @@
+from app.parsers import utils
+
+def test_remove_trailing_brackets():
+    word = "அம்மா (ammā)"
+    result = utils.remove_trailing_brackets(word)
+    expected = "அம்மா"
+    assert result == expected
+
+    word = "அம்மா (1234) (ammā)"
+    result = utils.remove_trailing_brackets(word)
+    expected = "அம்மா"
+    assert result == expected
