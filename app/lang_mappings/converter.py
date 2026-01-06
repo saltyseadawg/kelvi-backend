87ebb579dfd7760937ad1b0f622cbf30269ad23e
saltyseadawg <sabrinayu7@gmail.com>
2026-01-05 22:33:44 -0500

    refactor to use g2p for converting between language mappings

---
+++ app/lang_mappings/converter.py
@@ -0,0 +1,9 @@
+from g2p import make_g2p
+
+class Converter:
+    def __init__(self, in_lang: str, out_lang: str):
+        self.transducer = make_g2p(in_lang, out_lang)
+
+    def convert(self, input: str):
+        return self.transducer(input).output_string
+
